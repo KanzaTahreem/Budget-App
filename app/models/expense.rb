649f743012fe46207863d6f5cd933053ba580c81
KanzaTahreem <tehreemmsk411@gmail.com>
2023-02-14 01:13:20 +0500

    Add validations to the models

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,4 +1,8 @@
 class Expense < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
   has_many :group_expenses, class_name: 'GroupExpense', foreign_key: 'expense_id'
+
+  validates :name, presence: true, length: { in: 2..300 }
+  validates :amount, presence: true, numericality: { only_decimal: true, greater_than_or_equal_to: 0 }
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_many :groups, class_name: 'Group', foreign_key: 'user_id'
   has_many :expenses, class_name: 'Expense', foreign_key: 'user_id'
+
+  validates :name, presence: true, length: { in: 2..300 }
 end
