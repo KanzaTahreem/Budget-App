d2ead037edead2f66e4b8f805d9ffa1c638a19dc
KanzaTahreem <tehreemmsk411@gmail.com>
2023-02-14 01:39:50 +0500

    Enable confirmable in devise configuration

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
+  
   has_many :groups, class_name: 'Group', foreign_key: 'user_id'
   has_many :expenses, class_name: 'Expense', foreign_key: 'user_id'
 
