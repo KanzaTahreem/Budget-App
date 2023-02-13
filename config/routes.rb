a781419a5328540cd90b56cf22efa5f1bb451d2a
KanzaTahreem <tehreemmsk411@gmail.com>
2023-02-14 01:38:05 +0500

    Change devise configuration options

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :splash, only: [:index]
   # Defines the root path route ("/")
   # root "articles#index"
-  root 'splash/index'
+  root 'splash#index'
 end
