6b12a183e660135aa5f9614b1e702edba17acaf1
KanzaTahreem <tehreemmsk411@gmail.com>
2023-02-16 00:37:44 +0500

    Add an index action to get all groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,17 @@
 class GroupsController < ApplicationController
+  before_action :find_user
+
+  def index
+    @groups = @user.groups.all
+  end
+
+  private
+
+  def find_user
+    @user = current_user
+  end
+
+  def group_params
+    params.require(:group).permit(:name, :icon)
+  end
 end
