db45c5ef35c9d0c3cc77bf3b30c7c1fb69c8e069
KanzaTahreem <tehreemmsk411@gmail.com>
2023-02-16 00:29:30 +0500

    Allow user to upload an image

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,8 @@
 class Group < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
   has_many :group_expenses, class_name: 'GroupExpense', foreign_key: 'group_id'
+  
+  has_one_attached :icon
 
   validates :name, presence: true, length: { in: 2..300 }
 end
