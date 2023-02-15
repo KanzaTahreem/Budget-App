290857743fb817ce56fe93f5d17fba4c16497b58
KanzaTahreem <tehreemmsk411@gmail.com>
2023-02-15 16:15:20 +0500

    Add model validation specs

---
+++ spec/models/group_spec.rb
@@ -0,0 +1,72 @@
+require 'rails_helper'
+
+RSpec.describe Group, type: :model do
+  subject do
+    @user = User.create!(name: 'someone', email: 'user@user.com', password: 'password', id: 1)
+    @group = Group.create(name: 'stationary', icon: 'image.png', user: @user, user_id: 1, id: 1)
+  end
+
+  before { subject.save }
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+  
+  it 'is not valid if any empty attribute' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'name should be present' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'name should not be too short' do
+    subject.name = 'a'
+    expect(subject).to_not be_valid
+  end
+
+  it 'name should not be too long' do
+    subject.name = 'a' * 301
+    expect(subject).to_not be_valid
+  end
+
+  it 'name should not be empty' do
+    subject.name = ' '
+    expect(subject).to_not be_valid
+  end
+
+  it 'name should have valid value' do
+    expect(subject.name).to eql 'stationary'
+  end
+
+  it 'icon should be present' do
+    subject.icon = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'icon should be a string' do
+    expect(subject.icon).to be_an(String)
+  end
+
+  it 'should have the correct format' do
+    subject.icon = 'image.svg'
+    expect(subject).to_not be_valid
+  end
+
+  it 'user should be present' do
+    subject.user = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'user_id should be present' do
+    subject.user_id = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'user_id not be empty' do
+    subject.user_id = ' '
+    expect(subject).to_not be_valid
+  end
+end
