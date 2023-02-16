61c4032015d6efe1d36262330a912541a8963adb
KanzaTahreem <tehreemmsk411@gmail.com>
2023-02-17 03:16:03 +0500

    Create actions to create new expenses

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -6,6 +6,22 @@ class ExpensesController < ApplicationController
   def index
   end
 
+  def new
+    @expense = Expense.new
+  end
+
+  def create
+    @expense = Expense.new(expense_params)
+    @expense.user = @user
+    if @expense.save
+      GroupExpense.create(group_id: @group.id, expense_id: @expense.id)
+      redirect_to group_expenses_path(group_id: @group.id), notice: 'Expense created successfully'
+    else
+      flash.now[:alert] = @expense.errors.full_messages.first if @expense.errors.any?
+      render :new, status: 400
+    end
+  end
+
   private
   
   def find_user
