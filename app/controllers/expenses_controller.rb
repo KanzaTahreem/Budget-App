6413b8ce6967dfe5fed928fdcede9ff2fddb3479
KanzaTahreem <tehreemmsk411@gmail.com>
2023-02-17 03:12:27 +0500

    Create an index action in expenses controller

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,2 +1,26 @@
 class ExpensesController < ApplicationController
+  before_action :find_user
+  before_action :find_group
+  before_action :find_group_expenses
+
+  def index
+  end
+
+  private
+  
+  def find_user
+    @user = current_user
+  end
+  
+  def find_group
+    @group = Group.find_by_id(params[:group_id])
+  end
+
+  def find_group_expenses
+    @group_expenses = GroupExpense.where({group_id: params[:group_id]})
+  end
+
+  def expense_params
+    params.require(:expense).permit(:name, :amount, :icon)
+  end
 end
