ea5aa5800fccb805a7edb014805b2f7ea0cae4d5
KanzaTahreem <tehreemmsk411@gmail.com>
2023-02-18 03:18:54 +0500

    Define admin in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,8 @@ class User < ApplicationRecord
   has_many :expenses, class_name: 'Expense', foreign_key: 'user_id'
 
   validates :name, presence: true, length: { in: 2..300 }
+
+  def admin?
+    role == 'admin'
+  end
 end
