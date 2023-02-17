d30b5fd4112b230803ae494d7ff7b940975424e3
KanzaTahreem <tehreemmsk411@gmail.com>
2023-02-18 04:20:54 +0500

    Add tests for not found and unauthorized controllers

--- spec/requests/not_found_spec.rb
+++ spec/requests/not_found_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 RSpec.describe "NotFounds", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    before(:each) { get not_found_index_path }
+
+    it 'returns http success' do
+      follow_redirect!
+      expect(response).to have_http_status(:ok)
+    end
   end
 end
--- spec/requests/unauthorized_spec.rb
+++ spec/requests/unauthorized_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 RSpec.describe "Unauthorizeds", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    before(:each) { get unauthorized_path }
+
+    it 'returns http success' do
+      follow_redirect!
+      expect(response).to have_http_status(:ok)
+    end
   end
 end
