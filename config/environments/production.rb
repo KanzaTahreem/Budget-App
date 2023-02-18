67edd2c03fd180047c91f4a186f1cc9b53e68956
KanzaTahreem <tehreemmsk411@gmail.com>
2023-02-18 21:59:05 +0500

    Add email configurations

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,22 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "Budget_App_production"
 
-  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_caching = true
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              ENV['MAIL_ADDRESS'],
+    port:                 587,
+    domain:               ENV['DOMAIN'],
+    user_name:            ENV['MAIL_USER_NAME'],
+    password:             ENV['MAIL_PASS'],
+    authentication:       'plain',
+    enable_starttls_auto: true 
+  }
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
