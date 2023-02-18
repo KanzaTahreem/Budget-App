ff6344264ef64edb73f24e20cdce62a3746ac156
KanzaTahreem <tehreemmsk411@gmail.com>
2023-02-18 21:23:45 +0500

    Add host name for deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.hosts << "budget-app-production-1f98.up.railway.app"
   # Code is not reloaded between requests.
   config.cache_classes = true
 
