b2ad281d25f7a81b23719a5fc9faacb2a208622d
KanzaTahreem <tehreemmsk411@gmail.com>
2023-02-18 22:34:22 +0500

    Fix email configurations remove port

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'budget-app-production-1f98.up.railway.app', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'budget-app-production-1f98.up.railway.app' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
